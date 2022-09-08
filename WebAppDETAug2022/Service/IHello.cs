c3ac82593d6faffc26e073606733aa2b781ad74b
ETG1 <ETG1@H9B1T72-SHEL>
2022-09-08 06:21:06 -0700

    add a new message

---
+++ WebAppDETAug2022/Service/IHello.cs
@@ -0,0 +1,25 @@
+﻿namespace WebAppDETAug2022.Service
+{
+    public interface IHello
+    {
+
+            string SayHello(string name);
+        }
+        public class Hello : IHello
+        {
+            public string SayHello(string name)
+            {
+                return $"Hello {name}, Wellcome to Asp.NET Core";
+            }
+        }
+        public class Hello2 : IHello
+        {
+            public string SayHello(string name)
+            {
+                return $"Hai ,Hello{name}, How is the day!!";
+            }
+        }
+    }
+
+
+
